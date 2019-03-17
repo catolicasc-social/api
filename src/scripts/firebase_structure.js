ad5415b6462336fef303385bae4b4cf85e673db3
cardosothiago <thiagocrdso@gmail.com>
2019-03-17 18:58:12 -0300

    adicionado arquivo de estrutura firebase

---
+++ src/scripts/firebase_structure.js
@@ -0,0 +1,106 @@
+export const produto = [
+    {
+        categoria:[
+            {
+                descrição: "",
+                nome: ""
+            }
+        ]
+    },
+    {
+        item:[
+            {
+                dataCompra: "",
+                dataValidade: "",
+                fornecedorToken: "",
+                preço: "",
+                quantidade: 0
+            }
+
+        ]
+  },
+  {
+        marca:[
+            {
+                nome: ""
+            }
+        ]
+  },
+  {
+      unidadeMedida:[
+          {
+              abreviação: "",
+              descrição: ""
+          }         
+      ]
+
+  }
+  minimoEstoque: 0,
+  nome: ""  
+];
+
+export const usuario = [
+    {
+        empresa:[
+            {
+                endereço:[
+                    {
+                        rua: ""
+                    }
+                ]
+            },
+            {
+                fornecedor:[
+                    {
+                        nome: "Fornecedor Z"
+                    },
+                    {
+                        produtos:[
+                            {
+                                nome: "Arroz",
+                                token: "arrozToken23x"
+                            }
+                        ]
+                    }
+                ]
+            },
+            {
+                restaurante:[
+                    {
+                        endereço:[
+                            {
+                                rua: ""
+                            }
+                        ]
+                    },
+                    {
+                        estoque:[
+                            {
+                                descrição: "",
+                                nome: ""
+                            }
+                        ]
+                    }
+                    restaurante: "restaurante x"
+                ]
+            }
+            ativo: true,
+            nome: "Padaria X"
+        ]
+        administrador: true,
+        ativo: true,
+        email: "",
+        nome: José,
+        papel: ""
+    },
+    {
+        empresa:[
+            {
+                nome: "Padaria X"
+            }
+        ]
+        email: "",
+        nome: "Maria",
+        papel: ""
+    }
+];
