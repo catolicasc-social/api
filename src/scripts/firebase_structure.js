c6365000bbc62701206205c6cd2866a23b1de3aa
Thiago Cardoso <thiagocrdso@gmail.com>
2019-03-29 21:52:34 -0300

    atualizacao de estrutura

--- src/scripts/firebase_structure.js
+++ src/scripts/firebase_structure.js
@@ -1,106 +1,106 @@
-export const produto = [
-    {
-        categoria:[
-            {
-                descrição: "",
-                nome: ""
-            }
-        ]
+export const banco = [
+    produto: [
+        {
+            categoria:[
+                {
+                    descrição: "",
+                    nome: ""
+                }
+            ]
+        },
+        {
+            item:[
+                {
+                    dataCompra: "",
+                    dataValidade: "",
+                    fornecedorToken: "",
+                    preço: "",
+                    quantidade: 0
+                }
+
+            ]
     },
     {
-        item:[
-            {
-                dataCompra: "",
-                dataValidade: "",
-                fornecedorToken: "",
-                preço: "",
-                quantidade: 0
-            }
-
-        ]
-  },
-  {
         marca:[
             {
                 nome: ""
             }
         ]
-  },
-  {
-      unidadeMedida:[
-          {
-              abreviação: "",
-              descrição: ""
-          }         
-      ]
-
-  },
-  minimoEstoque: 0,
-  nome: ""  
-];
-
-export const usuario = [
-    {
-        empresa:[
-            {
-                endereço:[
-                    {
-                        rua: ""
-                    }
-                ]
-            },
-            {
-                fornecedor:[
-                    {
-                        nome: "Fornecedor Z"
-                    },
-                    {
-                        produtos:[
-                            {
-                                nome: "Arroz",
-                                token: "arrozToken23x"
-                            }
-                        ]
-                    }
-                ]
-            },
-            {
-                restaurante:[
-                    {
-                        endereço:[
-                            {
-                                rua: ""
-                            }
-                        ]
-                    },
-                    {
-                        estoque:[
-                            {
-                                descrição: "",
-                                nome: ""
-                            }
-                        ]
-                    },
-                    restaurante: "restaurante x"
-                ]
-            }
-            ativo: true,
-            nome: "Padaria X"
-        ]
-        administrador: true,
-        ativo: true,
-        email: "",
-        nome: José,
-        papel: ""
     },
     {
-        empresa:[
+        unidadeMedida:[
             {
-                nome: "Padaria X"
+                abreviação: "",
+                descrição: ""
             }
-        ],
-        email: "",
-        nome: "Maria",
-        papel: ""
-    }
+        ]
+    },
+    minimoEstoque: 0,
+    nome: ""
+    ],
+    usuario: [
+        {
+            empresa:[
+                {
+                    endereço:[
+                        {
+                            rua: ""
+                        }
+                    ]
+                },
+                {
+                    fornecedor:[
+                        {
+                            nome: "Fornecedor Z"
+                        },
+                        {
+                            produtos:[
+                                {
+                                    nome: "Arroz",
+                                    token: "arrozToken23x"
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    restaurante:[
+                        {
+                            endereço:[
+                                {
+                                    rua: ""
+                                }
+                            ]
+                        },
+                        {
+                            estoque:[
+                                {
+                                    descrição: "",
+                                    nome: ""
+                                }
+                            ]
+                        },
+                        restaurante: "restaurante x"
+                    ]
+                }
+                ativo: true,
+                nome: "Padaria X"
+            ]
+            administrador: true,
+            ativo: true,
+            email: "",
+            nome: José,
+            papel: ""
+        },
+        {
+            empresa:[
+                {
+                    nome: "Padaria X"
+                }
+            ],
+            email: "",
+            nome: "Maria",
+            papel: ""
+        }
+    ]
 ];
