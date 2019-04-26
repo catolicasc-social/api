22d69ef2d755f8fe0877d253d03b84de3d7e161c
Luis Felipe Manfroni <manfroni.luis@gmail.com>
2019-04-26 00:22:23 -0300

    Adaptando api para a nova configuração.

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const app = require('./src/app');
 const config = require('./config');
 
-app.listen(config.port, () => {
+app.listen(config.EXPRESS_CONFIG.PORT, () => {
   const port = app.get('port');
-  console.log(`API server running on port ${port} (http://localhost:${config.port})`);
+  console.log(`API server running on port ${port} (http://localhost:${port})`);
 });
 
 module.exports = app;
--- src/app.js
+++ src/app.js
@@ -1,13 +1,13 @@
 const express = require('express');
 require('dotenv').config();
 
-const config = require('../config');
+const config = require('../config').EXPRESS_CONFIG;
 const routes = require('./routes');
 
 const app = express();
 
 app.config = config;
-app.set('port', config.port);
+app.set('port', config.PORT);
 
 app.use(routes());
 
