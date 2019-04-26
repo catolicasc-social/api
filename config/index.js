cb1702c511145aa14fd067d46143078e96572208
Luis Felipe Manfroni <manfroni.luis@gmail.com>
2019-04-26 00:21:07 -0300

    Alterado arquivo de configuração para guardar informações do banco.

--- config/index.js
+++ config/index.js
@@ -1,3 +1,11 @@
 module.exports = {
-    port: process.env.PORT
+    EXPRESS_CONFIG: {
+        PORT: process.env.PORT,
+    },
+    FIREBASE_PRODUCAO: {
+      },
+    FIREBASE_DESENVOLVIMENTO: {
+      }
 };
+
+module.exports.firebase = module.exports["FIREBASE_" + process.env.BANCO];
