54c633c013a1aff6557189bed37be90eac6bfa6c
Luis Felipe Manfroni <luis.manfroni@hotmail.com>
2019-05-24 20:19:38 -0300

    Alterado log para ingles

--- src/controllers/products/delete.js
+++ src/controllers/products/delete.js
@@ -2,7 +2,7 @@ const firestore = require("../../services/firestore");
 
 module.exports = (req, res) => {
     let productId = req.params.id;
-    let message = `Produto ${ productId } deletado!`;
+    let message = `Product ${ productId } was deleted!`;
     //firestore.collection("produto").get(productId).delete().then( () => 
     res.json({msg: message});
     console.log(message);
